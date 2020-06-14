23600ad541c322c5c74e1486de15ac36fb0e43ef
Ben Woodward <ben@benwoodward.me.uk>
2020-06-14 21:56:39 +0100

    feat: add go-micro

--- shippy-cli-consignment/main.go
+++ shippy-cli-consignment/main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 
 	pb "github.com/Sacro/shippy/shippy-service-consignment/proto/consignment"
-	"google.golang.org/grpc"
+	"github.com/micro/go-micro/v2"
 )
 
 const (
@@ -27,13 +27,10 @@ func parseFile(file string) (*pb.Consignment, error) {
 }
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewShippingServiceClient(conn)
+	service := micro.NewService(micro.Name("shippy.consignment.cli"))
+	service.Init()
+
+	client := pb.NewShippingService("shippy.consignment.service", service.Client())
 
 	// Contact the server and print out its response.
 	file := defaultFilename
